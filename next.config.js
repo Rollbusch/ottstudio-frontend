dff2a7d4e861877c281337f3de7b34a1685f9790
joao <rollbuschplay@hotmail.com>
2023-10-03 19:55:17 -0300

    feat: subindo pagina temporaria

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/:page((?!em-breve$).*)",
+        destination: '/em-breve',
+        permanent: false
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
---
+++ src/components/WorkingInProgress/index.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import styles from "./index.module.css";
+
+import Logo from "./assets/logo-white.svg";
+import LogoBehance from "./assets/logo-behance-green.svg";
+
+export default function Container({ children }) {
+  return (
+    <main className={styles.wip}>
+      <Image className={styles.logo} src={Logo} alt="logo" />
+      <div className={styles.content}>
+        <h4>EM BREVE <br/> AQUI!</h4>
+        <p>BRANDING</p>
+        <p>IDENTIDADE VISUAL</p>
+        <p>ESTRATÉGIA E GESTÃO</p>
+      </div>
+      <div className={styles.contato}>
+        <a href="mailto:contato@ottbrands.com">contato@ottbrands.com</a>
+        <a href="https://www.instagram.com/aprendasobrelogo/" target="_blank">@aprendasobrelogo</a>
+        <a href="https://www.behance.net/ottstudio" target="_blank">
+          <Image src={LogoBehance} alt="behance" />
+        </a>
+      </div>
+    </main>
+  );
+}
